c4efed859e4a71a64d1e6d918fc55515cf629f0b
addy360 <abbyboy360@gmail.com>
2021-07-20 22:25:26 +0300

    headings

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
